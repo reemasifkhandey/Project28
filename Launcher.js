ee063ad775535c26740c46199dbc56581b9457d8
reemasifkhandey <73415822+reemasifkhandey@users.noreply.github.com>
2021-05-06 15:10:19 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,33 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness:0.004,
+            length: 1
+        }
+        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+    this.sling.bodyA=body;
+    }
+    display(){
+   
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push ()
+            stroke(49, 22, 8);
+                strokeWeight(7);
+                line (pointB.x,pointB.y,pointA.x,pointA.y)
+          pop ()
+        }
+    }
+}
+           
